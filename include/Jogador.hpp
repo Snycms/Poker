de127b5cc2c474c7be3c305403e0e5534d9e8103
Sanny Moreira <78819896+Snycms@users.noreply.github.com>
2023-06-15 23:04:26 -0300

    Add classes

---
+++ include/Jogador.hpp
@@ -0,0 +1,74 @@
+#ifndef JOGADORES_HPP
+#define JOGADORES_HPP
+
+#include "Mao.hpp"
+
+#include <string>
+
+class Jogador{
+    private:
+        bool _esta_jogando;
+        std::string _nome;
+        int _fichas;
+        Mao _mao;
+        int _num_vitoria;
+        //Pode ser Dealer, Small Blind ou Big Blind
+        std::string _tipo; 
+    
+    public:
+        Jogador();
+        ~Jogador();
+        
+        //Acoes que os jogadores podem realizar----------------------
+
+        //Passa a vez sem apostar(CHECK) e desistir(DESISTIR)
+        int check_desistir(std::string acao, int jogador_atual);
+
+        //Tipos de aposta:
+        //Aposta normal - Aumentar aposta
+        //Call(Aposta mesma quantidade que o ultimo jogador)
+        //All-in(Aposta tudo)
+
+        int aposta(std::string tipo_aposta, int valor_aposta);
+
+        //------------------------------------------------------------
+
+        void limpar_mesa();
+        
+        //TESTE GDB ONLINE
+        void exibe_jogador();
+        
+        //GETTERS E SETTERS ---------------------------------------
+    
+        //Get estado do Jogador
+        bool getEsta_jogando(); 
+        //Set estado do Jogador
+        void setEsta_jogando(bool esta_jogando); 
+
+        //Get nome do Jogador
+        std::string getNome();
+        //Set nome do Jogador
+        void setNome(std::string nome); 
+
+        //Get numero de fichas do Jogador
+        int getFichas(); 
+        //Set numero de fichas do Jogador
+        void setFichas(int fichas); 
+
+        //Get numero de vitorias do Jogador
+        int getNum_vitoria(); 
+        //Set numero de vitorias do Jogador
+        void setNum_vitoria(int num_vitoria); 
+
+        //Get objeto mao do Jogador
+        Mao &getObjeto_mao_jogador();
+        //Get vector mao do Jogador
+        std::vector<Carta> &getVector_mao_jogador();
+    
+        //Get tipo de Jogador(Dealer, Small Blind ou Big Blind)
+        std::string getTipo();
+        //Set tipo de Jogador(Dealer, Small Blind ou Big Blind)
+        void setTipo(std::string tipo);
+};
+
+#endif
---
+++ include/Mao.hpp
@@ -0,0 +1,154 @@
+#ifndef MAO_HPP
+#define MAO_HPP
+
+#include "Carta.hpp"
+
+#include <vector>
+#include <string>
+
+class Mao{
+  protected:
+    std::vector<Carta> _mao;
+  
+  public:
+    //Inicia mao dos jogadores
+    Mao();
+    //Destrutor
+    ~Mao();
+
+    //Adiciona mais cartas no flop, turn e river = setMao
+    void adiciona_carta(Carta carta); 
+
+    //Compara as cartas da mao de um jogador e retorna a maior
+    Carta maior_carta(); 
+    
+    //ordena as cartas da mão do jogador
+    void ordena_cartas();
+
+    //Retorna valor da mao do jogador
+    std::string valor_mao();
+
+    //confere se as cartas são do mesmo naipe
+     bool mesmo_naipe();
+
+     //confere se as cartas estão em sequencia
+     bool sequencia();
+
+     int mesmo_valor();
+    
+    virtual bool combina() const = 0;
+    
+     }
+
+
+
+    class Royal_flush : public Mao{
+      public:
+
+      //compara se a mão do jogador é um Royal Flush
+      bool combina() const override;
+    }
+
+
+
+    class Straight_flush : public Mao{
+       public:
+
+      //compara se a mão do jogador é um Straight Flush
+      bool combina() const override;
+    }
+
+
+    class Quadra : public Mao{
+       public:
+
+      //compara se a mão do jogador é uma quadra
+      bool combina() const override;
+    }
+
+
+
+    class Full_house : public Mao{ public:
+
+      //compara se a mão do jogador é um Full House
+      bool combina() const override;
+    }
+
+
+
+    class Flush : public Mao{ 
+      public:
+
+      //compara se a mão do jogador é um Flush
+      bool combina() const override;
+    }
+
+
+
+    class Trinca : public Mao{
+      public:
+
+      //compara se a mão do jogador é uma trinca
+      bool combina() const override;
+    }
+
+
+
+    class Dois_pares : public Mao{
+      public:
+
+      //compara se a mão do jogador tem dois pares
+      bool combina() const override;
+    }
+
+
+
+    class Par : public Mao{ 
+      public:
+
+      //compara se a mão do jogador tem um par
+      bool combina() const override;
+    }
+
+
+
+
+    class Carta_alta : public Mao{ 
+      public:
+
+      //compara se a mão do jogador tem a carta mais alta
+      bool combina() const override;
+    }
+
+
+
+    /* 
+    //POSSIVEIS MAOS--------------------------------------------
+
+    //Royal Flush e Straight Flush
+    int royal_flush_e_straight_flush();
+    //Quadra e Full House
+    int quadra_e_fullHouse();
+    //Flush
+    int flush();
+    //Straight
+    int straight();
+    //Trinca, Dois Pares, Par e Carta Alta
+    int trinca_doisPares_par_cartaAlta();
+
+    //----------------------------------------------------------
+
+    //TESTE ONLINE GDB
+    void exibe_mao();
+    
+    //GETTERS E AUXILIARES---------------------------------------
+    
+    //Ordena as cartas da mao(maior para a menor)
+    static bool ordena_mao_valor(Carta x, Carta y);
+
+    //Get mao
+    std::vector<Carta> &getMao();*/
+
+};
+
+#endif
