4ca06283ccc06bf992377953e173cfd78d0d5c46
Sanny Moreira <78819896+Snycms@users.noreply.github.com>
2023-07-03 14:46:21 -0300

    Update Poker_Game.cpp

--- src/Poker_Game.cpp
+++ src/Poker_Game.cpp
@@ -1,74 +1,49 @@
-#ifndef POKER_GAME_CPP
-#define POKER_GAME_CPP
+#ifndef POKER_GAME_HPP
+#define POKER_GAME_HPP
 
-#include "Poker_Game.hpp"
+#include "Carta.hpp"
+#include "Mao.hpp"
+#include "Baralho.hpp"
+#include "Jogador.hpp"
+#include "Computador.hpp"
 
-Poker_Game::Poker_Game(){
-    Baralho _baralho;
-    Jogador _jogador;
-    Computador _bot;
-    int POT = 0; //Premio do vencedor(Soma das apostas)
-    int aposta_anterior = 0;
-    int _jogador_atual = 0;
-    int _rodada = 0;
-}
+class Poker_Game{
+    private:
+        Baralho _baralho;
+        Jogador _jogador;
+        Computador _bot;
+        int _POT; //Premio do vencedor(Soma das apostas)
+        int _aposta_anterior;
+        int _jogador_atual;
+        int _rodada;
+    
+    public:
+        Poker_Game();
+        ~Poker_Game();
 
-Poker_Game::~Poker_Game(){
-    _baralho.~Baralho();
-    _jogador.~Jogador();
-    _bot.~Computador();
-}
+        //Parte final do jogo
+        std::string showdown(Jogador &jogador, Jogador &bot);
 
-std::string Poker_Game::showdown(Jogador &jogador, Jogador &bot){
-    jogador.virar_cartas();
-    bot.virar_cartas();
+        Baralho &limpar_mesa(Baralho &baralho, Jogador &jogador, Computador &bot);
 
-    int mao_jogador = jogador.getObjeto_mao_jogador().determinar_valor_mao();
-    int mao_bot = bot.getObjeto_mao_jogador().determinar_valor_mao();
+        //GETTERS E SETTERS ---------------------------------------
 
-    if(mao_jogador > mao_bot){
-        jogador.adicionar_fichas(getPOT());
-        return "JOGADOR VENCEU";;
-    }
-    else if(mao_bot > mao_jogador){
-        bot.adicionar_fichas(getPOT());
-        return "BOT VENCEU";
-    }
-    else{ //mao_jogador == mao_bot 
-        jogador.adicionar_fichas(getPOT()/2);
-        bot.adicionar_fichas(getPOT()/2);
-        return "JOGO EMPATADO";
-    }
-}
+        Baralho &getBaralho();
+        Jogador &getJogador();
+        Computador &getBot();
 
-//Limpa tudo e retorna um novo baralho
-Baralho &Poker_Game::limpar_mesa(Baralho &baralho, Jogador &jogador, Computador &bot){
-    baralho.~Baralho();
-    jogador.getVector_mao_jogador().clear();
-    bot.getVector_mao_jogador().clear();
-    setPOT(0);
-    // Baralho baralho;
-    return baralho;
-}
+        int getPOT();
+        void adiciona_aposta_POT(int valor_aposta);
+        void setPOT(int valor_aposta);
 
-//GETTERS E SETTERS ---------------------------------------
-        
-Baralho &Poker_Game::getBaralho(){return _baralho;}
-Jogador &Poker_Game::getJogador(){return _jogador;}
-Computador &Poker_Game::getBot(){return _bot;}
+        int getAposta_anterior();
+        void setAposta_anterior(int aposta_anterior);
 
-int Poker_Game::getPOT(){return _POT;}
-void Poker_Game::adiciona_aposta_POT(int valor_aposta){_POT += valor_aposta;}
-void Poker_Game::setPOT(int POT){_POT = POT;}
+        int getJogador_atual();
+        void setJogador_atual(int jogador_atual);
 
-int Poker_Game::getAposta_anterior(){return _aposta_anterior;}
-void Poker_Game::setAposta_anterior(int aposta_anterior){_aposta_anterior = aposta_anterior;}
-
-int Poker_Game::getJogador_atual(){return _jogador_atual;}
-void Poker_Game::setJogador_atual(int jogador_atual){_jogador_atual = jogador_atual;}
-
-int Poker_Game::getRodada(){return _rodada;}
-void Poker_Game::setRodada(int rodada){_rodada = rodada;}
-
+        int getRodada();
+        void setRodada(int rodada);
+};
 
 #endif
