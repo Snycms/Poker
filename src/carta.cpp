85117462b0237d490b95308223d766ff7bea590b
Pedro Dalla-Lana <55250336+Cryomanc3r@users.noreply.github.com>
2023-07-03 20:51:49 -0300

    Add files via upload
    Funções que tavam faltando

--- src/carta.cpp
+++ src/carta.cpp
@@ -26,7 +26,12 @@ std::string Carta::getNaipe(){return _naipe;}
 bool Carta::getRosto_baixo(){return _rosto_baixo;}
 void Carta::setRosto_baixo(bool rosto_baixo){_rosto_baixo = rosto_baixo;}
 
-bool operator>(const Carta& other) const {
+void Carta::exibe_carta(){
+    std::cout << "Nome: " << getNome() << " - ";
+    std::cout << "Naipe: " << getNaipe() << std::endl;
+};
+
+bool Carta::operator > (const Carta& other) const {
         return _valor > other._valor;
     }
 
